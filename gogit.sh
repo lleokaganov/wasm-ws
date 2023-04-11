245cac3b05ab938df91120fb8568a9e30565fdc8
Leonid Kaganov <lleo@lleo.me>
2023-04-11 16:15:44 +0300

    major: init

--- gogit.sh
+++ gogit.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# change .git/config:
+#        #url = https://github.com/Alzymologist/wasm-js.git
+#        url = git@github.com:Alzymologist/wasm-js.git
+
 #git clone git@github.com:Alzymologist/shave-rust.git
 git status
 git add .
